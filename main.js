025cbc689ee1f1814b9fbcbc010e603f6e518e33
August Miller <augustamiller77@gmail.com>
2023-09-24 09:59:17 -0500

    Added poems and script construct map

---
+++ main.js
@@ -0,0 +1,41 @@
+const fs = require('fs');
+
+// Read the input text document
+const inputText = fs.readFileSync('rawData.txt', 'utf8');
+
+// Split the text into sections
+const sections = inputText.split('[title]\r\n');
+
+// Process each section
+
+var searchMap = {};
+
+sections.forEach(section => {
+    const [title, body] = section.split('[body]\r\n');
+    if (title !== "") {
+        const titleText = title.trim();
+        const bodyText = body.trim().split('\r\n');
+
+        // Create JSON data
+        const jsonData = {
+            title: titleText,
+            body: bodyText
+        };
+
+        // Generate the output filename
+
+        var filename = titleText.trim().toLowerCase().replace(/[^a-z0-9]+/g, '-');
+        while (searchMap[filename + '.json'] != null) {
+            filename = filename + "-2";
+        }
+
+        filename = filename + '.json';
+
+        searchMap[filename] = titleText;
+        // Write the JSON data to a file
+        fs.writeFileSync("./data/" + filename, JSON.stringify(jsonData, null, 2));
+    }
+});
+
+//write the search map file
+fs.writeFileSync("searchMap.json", JSON.stringify(searchMap));
